cneves/openmicroscopy

changing SERVER_LIST.find to allow keyword args

components/tools/OmeroWeb/omeroweb/webadmin/custom_models.py
@@ -68,11 +68,14 @@ class ServerObjects(object):
                     return b
         return None
     
-    def find(self, server_host):
+    def find(self, **kwargs):
+        rv = []
         for b in self.blitz_list:
-            if b.host == server_host:
-                return b
-        return None
+            for k,v in kwargs.items():
+                if getattr(b, k) != v:
+                    continue
+                rv.append(b)
+        return rv
     
     def all(self):
         return self.blitz_list

joshmoore/omero-web

Former-commit-id: 128ea2171bb4be480c2b950325b05ffb1ae6940d

omeroweb/webadmin/custom_models.py
@@ -98,10 +98,14 @@ class Server(ServerBase):
             if cls._registry.has_key(pk):
                 r = cls._registry[pk]
         return r
-
+    
     @classmethod
-    def find(cls, server_host):
+    def find(cls, **kwargs):
+        rv = []
         for s in cls._registry.values():
-            if unicode(s.host) == unicode(server_host):
-                return s
-        return None
+            for k,v in kwargs.items():
+                if getattr(s, k) != v:
+                    continue
+                rv.append(s)
+        return rv
+